053907137b225428a57326b7f30fc0352b61a659
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-12-11 03:33:18 +0000

    Add glider to console debug.

--- src/com/laurence_rawlings/Debug.java
+++ src/com/laurence_rawlings/Debug.java
@@ -1,20 +1,28 @@
 package com.laurence_rawlings;
 
+import java.io.IOException;
+
 public class Debug {
 
     public static void main(String[] args) {
 	    Board b = new Board(50, 10);
-	    System.out.println(b);
 
+	    //Glider
 	    b.createCell(10, 5);
         b.createCell(10, 6);
         b.createCell(10, 7);
-	    System.out.println(b);
+        b.createCell(9, 7);
+        b.createCell(8, 6);
 
-	    b.updateBoard();
-	    System.out.println(b);
-
-        b.updateBoard();
-        System.out.println(b);
+        //Draw Loop
+        while (true) {
+            System.out.println(b);
+            b.updateBoard();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
